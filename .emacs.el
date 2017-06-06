0b2c680f5dd591ab314325564e1d89b1ef7c683c
Luke W. McCullough (Yoda Book) <lukaswm@gmail.com>
2017-06-06 11:58:11 -0400

    most upto date emacs config from yoda-book

--- .emacs.el
+++ .emacs.el
@@ -6,12 +6,17 @@
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
+(require 'use-package)
+
+(use-package langtool :ensure t :defer t)
+(setq langtool-language-tool-jar "D:/LanguageTool-3.7/languagetool-commandline.jar")
+(setq langtool-default-language "en-US")
 
 ;; improved file and buffer open stuff
-(require 'ido)
+(use-package ido :ensure t :defer t)
 (ido-mode t)
 
-(require 'wc-mode)
+(use-package wc-mode :ensure t :defer t)
 
 ;; turn off the bell
 (setq ring-bell-function 'ignore)
@@ -23,7 +28,7 @@
 (add-to-list 'exec-path "C:/Program Files (x86)/Aspell/bin/")
 (add-to-list 'exec-path "/usr/local/bin")
 
-;; wordstar mode
+;; my wordstar mode
 (require 'lws-mode)
 
 ;; use ansi colors in console mode
@@ -34,7 +39,7 @@
 
 ;; when in graphics, aka not console mode
 (when (display-graphic-p)
-  (load-theme 'solarized-light t)
+  (load-theme 'sanityinc-solarized-light t)
   (tool-bar-mode -1))
 
 ;; disable the startup screen
@@ -50,16 +55,17 @@
 (define-key global-map [end] 'end-of-line)
 
 ;; writing mode stuff
-;;(require 'sublimity)
-;; (require 'sublimity-scroll)
-;;(require 'sublimity-attractive)
+;;(use-package sublimity :ensure t :defer t)
+;; (use-package sublimity-scroll :ensure t :defer t)
+;;(use-package sublimity-attractive :ensure t :defer t)
 ;;(setq sublimity-attractive-centering-width 80)
 ;;(sublimity-mode 1)
-;;(require 'sublimity-map)
+;;(use-package sublimity-map :ensure t :defer t)
 (setq cwm-centered-window-width 85)
-(require 'centered-window-mode)
+(use-package centered-window-mode :ensure t :defer t)
 ;;(centered-window-mode t)
-(require 'mode-local)
+(use-package mode-local :ensure t :defer t)
+
 ;; increase spacing
 (defun ltoggle-line-spacing ()
   "Toggle line spacing"
@@ -68,6 +74,9 @@
   (redraw-frame (selected-frame)))
 
 (setq flyspell-issue-message-flag nil)
+(use-package writegood-mode :ensure t :defer t)
+
+;; auto modes
 ;;(setq-default major-mode 'text-mode)
 (add-hook 'wordstar-mode-hook 'ltoggle-line-spacing)
 (add-hook 'wordstar-mode-hook 'flyspell-mode)
@@ -79,7 +88,8 @@
 (defun my-flyspell-hook ()
   (define-key flyspell-mode-map "\C-c\$" nil)
   (define-key flyspell-mode-map "\C-c" nil)
-  (define-key wordstar-mode-map "\C-c" 'scroll-up-command))
+  (define-key wordstar-mode-map "\C-c" 'scroll-up-command)
+  (add-to-list 'ispell-extra-args "--sug-mode=ultra"))
 (add-hook 'flyspell-mode-hook 'my-flyspell-hook)
 
 (defun my-wc-hook ()
@@ -102,6 +112,8 @@
 (global-hl-line-mode +1)
 ;; spelling 
 (setq-default ispell-program-name "aspell")
+(use-package flyspell-lazy :ensure t :defer t)
+(flyspell-lazy-mode 1)
 
 ;; set the font
 (custom-set-faces
@@ -114,6 +126,9 @@
  '(fringe ((t (:background "#fdf6e3"))))
  '(org-todo ((t (:background "#c6c6c6" :foreground "#042028" :inverse-video nil :underline nil :slant normal :weight bold))))
  '(org-upcoming-deadline ((t (:foreground "highlightColor")))))
+
+
+
 
 ;; move autosave dir
 (setq backup-directory-alist `((".*" . ,temporary-file-directory)))
@@ -134,11 +149,16 @@
                                   ("%b - Dir:  " default-directory)))))))
 
 ;; Org mode stuff
+(use-package org :ensure t :defer t)
+(use-package org-autolist :ensure t :defer t)
+(use-package org-bullets :ensure t :defer t)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
+(setq org-startup-indented t)
 (setq org-log-done t)
 (setq org-agenda-files (list "~/Dropbox/org/top.org"))
 (setq org-log-done 'note)
@@ -185,6 +205,7 @@
    (vector "#657b83" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#fdf6e3"))
  '(background-color "#fcf4dc")
  '(background-mode light)
+ '(centered-window-mode t)
  '(compilation-message-face (quote default))
  '(compile-command
    "python /Users/lukas/code/Broadway/build2/scons-2.3.0/Scripts/scons --vars=/Users/lukas/code/Broadway/local-vars.py -U -s --verbose build")
@@ -193,9 +214,10 @@
  '(cua-overwrite-cursor-color "#b58900")
  '(cua-read-only-cursor-color "#859900")
  '(cursor-color "#52676f")
+ '(custom-enabled-themes (quote (sanityinc-solarized-light)))
  '(custom-safe-themes
    (quote
-    ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default)))
+    ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default)))
  '(fci-rule-color "#eee8d5")
  '(foreground-color "#52676f")
  '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
@@ -222,19 +244,23 @@
    (quote
     ("#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3" "#fdf6e3")))
  '(magit-diff-use-overlays nil)
- '(org-directory "~/Dropbox/org")
+ '(nrepl-message-colors
+   (quote
+    ("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4")))
+ '(org-directory "~/Dropbox/org" t)
  '(org-mobile-directory "~/Dropbox/Apps/MobileOrg" t)
  '(org-mobile-files org-agenda-files t)
  '(org-mobile-inbox-for-pull "~/Dropbox/org/top.org" t)
  '(package-selected-packages
    (quote
-    (habitica centered-window-mode magit wc-mode sublimity minimap solarized-theme)))
+    (flyspell-lazy company color-theme-sanityinc-solarized color-theme-solarized habitica centered-window-mode magit wc-mode sublimity minimap solarized-theme)))
  '(pos-tip-background-color "#eee8d5")
  '(pos-tip-foreground-color "#586e75")
  '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#eee8d5" 0.2))
  '(term-default-bg-color "#fdf6e3")
  '(term-default-fg-color "#657b83")
  '(vc-annotate-background nil)
+ '(vc-annotate-background-mode nil)
  '(vc-annotate-color-map
    (quote
     ((20 . "#dc322f")
@@ -262,5 +288,9 @@
     (("default" "http://technology-hates-you.com/lm3m/xmlrpc.php" "lm3m" "" "1"))))
  '(weechat-color-list
    (quote
-    (unspecified "#fdf6e3" "#eee8d5" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#657b83" "#839496"))))
+    (unspecified "#fdf6e3" "#eee8d5" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#657b83" "#839496")))
+ '(xterm-color-names
+   ["#eee8d5" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#073642"])
+ '(xterm-color-names-bright
+   ["#fdf6e3" "#cb4b16" "#93a1a1" "#839496" "#657b83" "#6c71c4" "#586e75" "#002b36"]))
 
